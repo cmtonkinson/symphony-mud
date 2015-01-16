eb1b71b4fba0a9190bfb5316943430d10f5d1c73
Chris Tonkinson <chris@tonkinson.com>
2015-01-16 12:51:57 -0500

    Sort (and add one) quote

--- src/quoteTable.cpp
+++ src/quoteTable.cpp
@@ -2,8 +2,8 @@
 #include "quoteTable.h"
 
 QuoteTable::QuoteTable(void) {
-  quotes().push_back(Quote("Arthur C. Clarke", "Any sufficiently advanced technology is indistinguishable from magic."));
   quotes().push_back(Quote("Albert Einstein", "The significant problems we face cannot be solved by the same level of thinking that created them."));
+  quotes().push_back(Quote("Arthur C. Clarke", "Any sufficiently advanced technology is indistinguishable from magic."));
   quotes().push_back(Quote("Bill Bryson", "A computer is a stupid machine with the ability to do incredibly smart things, while computer programmers are smart people with the ability to do incredibly stupid things.  They are, in short, a perfect match."));
   quotes().push_back(Quote("Brian Kernighan", "Debugging is twice as hard as writing the code in the first place.  Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it."));
   quotes().push_back(Quote("Charles Petzold", "Owning a computer without programming is like having a kitchen and using only the microwave oven."));
@@ -20,16 +20,11 @@ QuoteTable::QuoteTable(void) {
   quotes().push_back(Quote("Fred Brooks", "Nine people can't make a baby in a month."));
   quotes().push_back(Quote("Gavin Russell Baker", "C++ : Where friends have access to your private members."));
   quotes().push_back(Quote("Graham Storr", "Program, n.: (1) A magic spell cast upon a computer to enable it to turn your input into error messages. (2) v.t., A pastime similar to banging your head against a wall but with fewer opportunities for reward."));
-  quotes().push_back(Quote("The Guild", "Blades: \"Wiggley, you don't have an arsenal.  You're in a pit of noob and you can't get out.\""));
-  quotes().push_back(Quote("The Guild", "Codex: \"You can't be bald and confident?\"\nClara: \"Um... your skull is out all the time.\""));
-  quotes().push_back(Quote("The Guild", "Vork: \"You will be dead to me as a person.  So much so, that were I faced with the decision of saving you or a strange stray dog that I'd never met before from drowning, I would do my best to drown you myself to save the strange dog that I would not even care to keep, but would, afterwards, place in a non-abusive foster home.  Good day, sir!\""));
-  quotes().push_back(Quote("The Guild", "Zaboo: \"There is so much fail in that sentence I can hardly begin.\""));
   quotes().push_back(Quote("Howard Aiken", "Don't worry about people stealing your ideas.  If it's original, you'll have to ram it down their throats."));
   quotes().push_back(Quote("Isaac Asimov", "The most exciting phrase to hear in science, the one that heralds new discoveries, is not 'Eureka!' but 'That's funny...'"));
   quotes().push_back(Quote("J. Porter Clark", "... sufficiently advanced cluelessness is indistinguishable from malice."));
   quotes().push_back(Quote("Jeff Atwood", "Good programmers never write what they can steal or borrow."));
   quotes().push_back(Quote("Jeremy S. Anderson", "There are two major products that come out of Berkeley: LSD and UNIX. We don't believe this to be a coincidence."));
-  quotes().push_back(Quote("The Oregon Trail", "You have died of dysentery."));
   quotes().push_back(Quote("John Ousterhout", "The greatest performance improvement of all is when a system goes from not-working to working."));
   quotes().push_back(Quote("Keith Bostic", "Perl - The only language that looks the same before and after RSA encryption."));
   quotes().push_back(Quote("Martin Fowler", "Any fool can write code that a computer can understand. Good programmers write code that humans can understand."));
@@ -40,23 +35,29 @@ QuoteTable::QuoteTable(void) {
   quotes().push_back(Quote("Rich Cook", "Programming today is a race between software engineers striving to build bigger and better idiot-proof programs, and the Universe trying to produce bigger and better idiots.  So far, the Universe is winning."));
   quotes().push_back(Quote("Robert Sewell", "If Java had true garbage collection, most programs would delete themselves upon execution."));
   quotes().push_back(Quote("Steve Ballmer", "Linux is a cancer."));
+  quotes().push_back(Quote("The Guild", "Blades: \"Wiggley, you don't have an arsenal.  You're in a pit of noob and you can't get out.\""));
+  quotes().push_back(Quote("The Guild", "Codex: \"You can't be bald and confident?\"\nClara: \"Um... your skull is out all the time.\""));
+  quotes().push_back(Quote("The Guild", "Vork: \"You will be dead to me as a person.  So much so, that were I faced with the decision of saving you or a strange stray dog that I'd never met before from drowning, I would do my best to drown you myself to save the strange dog that I would not even care to keep, but would, afterwards, place in a non-abusive foster home.  Good day, sir!\""));
+  quotes().push_back(Quote("The Guild", "Zaboo: \"There is so much fail in that sentence I can hardly begin.\""));
+  quotes().push_back(Quote("The Oregon Trail", "You have died of dysentery."));
   quotes().push_back(Quote("Thomas A. Edison", "I have not failed. I've just found 10,000 ways that won't work."));
   quotes().push_back(Quote("Unknown", "A programmer started to cuss\nBecause getting to sleep was a fuss\nAs he lay there in bed\nLooping 'round in his head\nwas: while(!asleep()) sheep++;"));
   quotes().push_back(Quote("Unknown", "Before software can be reusable it first has to be usable."));
   quotes().push_back(Quote("Unknown", "Cheap, fast, reliable - pick 2."));
   quotes().push_back(Quote("Unknown", "Confidence, n.: The feeling you have before you understand the situation."));
   quotes().push_back(Quote("Unknown", "Debuggers don't remove bugs. They only show them in slow motion."));
+  quotes().push_back(Quote("Unknown", "It is better to be approximately right than exactly wrong."));
   quotes().push_back(Quote("Unknown", "Java. The elegant simplicity of C++. The blazing speed of Smalltalk."));
   quotes().push_back(Quote("Unknown", "Man is the best computer we can put aboard a spacecraft...and the only one that can be mass produced with unskilled labor."));
   quotes().push_back(Quote("Unknown", "One programmer can do in one day what two programmers can do in two."));
   quotes().push_back(Quote("Unknown", "Profanity is the one language all programmers know best."));
   quotes().push_back(Quote("Unknown", "Saying that Java is good because it works on all platforms is like saying anal sex is good because it works on all genders."));
-  quotes().push_back(Quote("Unknown", "To err is human; to make real mess, you need a computer."));
   quotes().push_back(Quote("Unknown", "The best code is no code at all."));
   quotes().push_back(Quote("Unknown", "Theory is when you know something, but it doesn't work.  Practice is when something works, but you don't know why.  Programmers combine theory and practice: Nothing works and they don't know why."));
   quotes().push_back(Quote("Unknown", "There are 10 types of people in this world.  Those who understand binary, and those who don't."));
   quotes().push_back(Quote("Unknown", "There are only two industries that refer to their customers as users."));
   quotes().push_back(Quote("Unknown", "There is not now, nor has there ever been, nor will there ever be, any programming language in which it is the least bit difficult to write bad code."));
+  quotes().push_back(Quote("Unknown", "To err is human; to make real mess, you need a computer."));
   quotes().push_back(Quote("Unknown", "Weeks of coding can save you hours of planning."));
   quotes().push_back(Quote("Unknown", "When all you have is a hammer, everything starts to look like a nail."));
   return;
